65f7c1420277390625d6307ce1b302a710d27589
The-Fuse <rohitkumaryadav141002@gmail.com>
2021-09-16 20:26:23 +0530

    cors added

--- api/leaderboard/controller.js
+++ api/leaderboard/controller.js
@@ -6,7 +6,6 @@ const Leaderboard = require('./model');
 
 exports.getLeaderboards = async (req,res,next) => {
     const leaderboardata = await Leaderboard.find();
-    res.header("Access-Control-Allow-Origin", "*")
     res.status(200).json({
         success: true,
         count: leaderboardata.length,
